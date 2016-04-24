00c8c2b59f982998d71c1f07a1b2e2155f443d84
Your Name <you@example.com>
2016-04-24 23:52:15 +0800

    none:

---
+++ LoginOut.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+public partial class LoginOut : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!this.Page.IsPostBack)
+        {
+            Response.Cookies["Login"].Value = "no";
+            Response.Write(MessageBox.ShowMessage("注消成功！", "Login.aspx"));
+        }
+    }
+}
